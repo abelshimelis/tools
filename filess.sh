87ba2e461b86535ba670aebd7b1f44c66e71f838
abelshimelis <141934895+abelshimelis@users.noreply.github.com>
2023-08-24 21:49:06 +0300

    Add files via upload

---
+++ filess.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+#SIMPLE WEB PENTESTER INSTALLER
+
+#Scanning ports and vulnurabilities
+sudo apt install nmap -y
+
+#best expoloitation tool
+sudo apt install msf console
+
+#burpsuite
+sudo apt install burp suite
+
+#Vulnurability scanner
+sudo apt install nikto
+
+#command line to upgarde & update tools
+sudo apt-get update && upgrade
+
+#performing brute force to the dir
+sudo apt install gobuster
+
+#ip address scanner 
+sudo apt install nslookup
+
+#power full vunerablity scanner
+sudo dpkg -i Nessus-10.5.4-ubuntu1404_amd64.deb 
+
+#to perform reverse shell
+sudo apt install netcut
+
+done
